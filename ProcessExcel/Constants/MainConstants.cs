ca8cc8f20264c5af0fc0da85e09571588fc662a5
Ramon Souza <ramon.souza@elevenfinancial.com>
2021-12-07 11:33:48 -0300

    other key

--- ProcessExcel/Constants/MainConstants.cs
+++ ProcessExcel/Constants/MainConstants.cs
@@ -6,6 +6,11 @@
         public const string urlBackBase = ""; 
         public const string MachineKey = "";
         public const string token = "";
-
+        public const int accountsId = 0;
+        public const string userGroupsId = "";
+        public const int Origins = 0;
+        public const int ProductsId = 0;
+        public const int ContractId = 0;
+        public const string FileName = "bla.xls";
     }
 }
